cf69fd1c0662f21297a08b4d3f30cc05f856fe1e
Nobertx <emmanuel2012123@gmail.com>
2021-09-13 16:51:27 -0300

    pratica

--- index.mjs
+++ index.mjs
@@ -20,7 +20,7 @@ app.get('/noel', (req, res) => {
   res.send('Hello Noel!')
 });
 
-app.post('/', (req, res) => {
+app.get('/', (req, res) => {
   res.send('Escolha a rota: /hello, /clientes, /noel')
 });
 
