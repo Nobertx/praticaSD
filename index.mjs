82c15c8b07d748149988f78302b29fc65f9d1126
Nobertx <emmanuel2012123@gmail.com>
2021-10-20 18:03:58 -0300

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,7 +1,13 @@
-import express from 'express';
+import express, {request} from 'express';
 
 
 const app = express()
+let info = {"server_name": "noel",
+  "server_endpoint": "https://sd-emmanuel.herokuapp.com/",
+  "descricao": "Projeto de SD. Os seguintes serviços estão implementados: info",
+  "versao": "0.1",
+  "status": "online",
+  "tipo_de_eleicao_ativa": "ring"}
 
 app.get('/hello', (req, res) => {
   if(req.query.name) {
@@ -23,6 +29,15 @@ app.get('/noel', (req, res) => {
 app.get('/', (req, res) => {
   res.send('Escolha a rota: /hello, /clientes, /noel')
 });
+
+app.get('/info', (req, res) => {
+
+  res.send(info);
+});
+
+app.put('/info',(req, res) => {
+
+})
 
 app.listen(process.env.PORT ||8000, () => {
   console.log('App Started...');
